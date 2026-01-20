83ff9fca14586f4f4abaf53450196ce5389a4dac
demi-code <112693204+clintoo@users.noreply.github.com>
2026-01-20 12:22:41 +0000

    Made changes to correct the avatar bug-fix

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export const InfiniteMovingCards = ({
@@ -117,7 +118,7 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 {/* add this div for the profile img */}
                 <div className="me-3">
-                  <img src={item.img} alt={item.name} />
+                  <Image src={item.img} alt={item.name} width={40} height={40} />
                 </div>
                 <span className="flex flex-col gap-1">
                   {/* change text color, font-normal to font-bold, text-xl */}
